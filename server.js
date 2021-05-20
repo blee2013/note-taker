4f142cb34c13b4614c04e5e1a5c0f05814fe4edb
Brittany Moes <brittanylee@Brittanys-MacBook-Pro.local>
2021-05-19 23:06:32 -0500

    redid structure

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// dependencies
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,15 +5,13 @@ const fs = require('fs');
 const path = require('path');
 const uuid = require('uuid');
 
-// Middleware
+
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-// display notes from db.json
 app.get('/api/notes', (req, res) => {
-    // res.sendFile(path.join(__dirname, './Develop/db/db.json'))
     const notes = fs.readFileSync('./Develop/db/db.json', 'utf8')
     console.log(notes);
     res.json(JSON.parse(notes));
@@ -22,7 +19,6 @@ app.get('/api/notes', (req, res) => {
 });
 
 
-// post to notes array
 app.post('/api/notes', (req, res) => {
     let notes = fs.readFileSync('./Develop/db/db.json', 'utf8')
     notes = JSON.parse(notes)
@@ -38,11 +34,10 @@ app.post('/api/notes', (req, res) => {
     res.json(notes);
 });
 
-// delete note from db
 app.delete('/api/notes/:id', (req, res) => {
 
     const notes = JSON.parse(fs.readFileSync('./Develop/db/db.json'));
-    // console.log(notes)
+
     const removeNote = notes.filter((deleteNote) => deleteNote.id !== req.params.id);
     console.log(removeNote)
     fs.writeFileSync('./Develop/db/db.json', JSON.stringify(removeNote));
@@ -51,17 +46,14 @@ app.delete('/api/notes/:id', (req, res) => {
 });
 
 
-// display notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-// display index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-// port listener
 app.listen(PORT, () => {
     console.log(`API server now on port http://localhost:${PORT}`);
 });
